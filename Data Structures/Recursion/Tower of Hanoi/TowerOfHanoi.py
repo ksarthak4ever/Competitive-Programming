f18eeabf19afd382dbd90ff9c2d091f6af95c20e
Sarthak Kumar <43024535+ksarthak4ever@users.noreply.github.com>
2019-01-10 22:02:04 +0530

    Tower of Hanoi

---
+++ Data Structures/Recursion/Tower of Hanoi/TowerOfHanoi.py
@@ -0,0 +1,12 @@
+def moveTower(height,fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height-1,fromPole,withPole,toPole)
+        moveDisk(fromPole,toPole)
+        moveTower(height-1,withPole,toPole,fromPole)
+
+def moveDisk(fp,tp):
+    print("moving disk from",fp,"to",tp)
+
+print("Enter the height of the tower")
+n = int(input())
+moveTower(n,"A","B","C")
