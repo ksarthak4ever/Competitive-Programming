48c55cff23693462c3cd516023a53ade7fbd375b
Sarthak Kumar <ksarthak4ever@gmail.com>
2019-04-05 18:58:22 +0530

    stack

---
+++ Data Structures/Stacks/Practice/stack.py
@@ -0,0 +1,30 @@
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)				
+
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        
+    def get_stack(self):
+        return self.items
+
+s = Stack()
+print(s.is_empty())
+s.push("A")
+s.push("B")
+s.push("C")
+s.push("D")
+print(s.get_stack())
+s.pop()
+print(s.get_stack())
+print(s.peek())
