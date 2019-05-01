6c57c22c11f29171c50bb2bc60b1ba0ca88e9f9a
Sarthak Kumar <ksarthak4ever@gmail.com>
2019-05-01 20:58:09 +0530

    integer to string

---
+++ Data Structures/Strings/String Processing - Integer to String/int_to_str.py
@@ -0,0 +1,29 @@
+def int_to_str(input_int):
+
+    if input_int < 0:
+        is_negative = True
+        input_int *= -1
+    else:
+        is_negative = False
+
+    output_str = []
+    while input_int > 0:
+        output_str.append(chr(ord('0') + input_int % 10))
+        input_int //= 10
+    output_str = output_str[::-1]
+
+    output_str = ''.join(output_str)
+
+    if is_negative:
+        return '-' + output_str
+    else:
+        return output_str
+
+
+input_int = 123
+
+print(type(input_int))
+
+output_str = int_to_str(input_int)
+
+print(type(output_str))
