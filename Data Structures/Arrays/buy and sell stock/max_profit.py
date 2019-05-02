681bd4abe5cc52bc56e96f49d275f45ef7d7902b
Sarthak Kumar <ksarthak4ever@gmail.com>
2019-05-02 23:16:48 +0530

    max profit in sales

---
+++ Data Structures/Arrays/buy and sell stock/max_profit.py
@@ -0,0 +1,16 @@
+prices = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def buy_and_sell_stock_once(prices):
+	max_profit = 0.0
+	min_price = float('inf')
+
+	for price in prices:
+		min_price = min(min_price, price)
+
+		compare_profit = price - min_price
+
+		max_profit = max(max_profit, compare_profit)
+
+	return max_profit
