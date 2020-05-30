87c9e4aa499d05ede27ad863ee48c18dd0cb8e43
Sarthak <Sarthak@shubhams-MacBook-Air.local>
2020-05-30 22:47:01 +0530

    basic stack implementation using lists in python

---
+++ HackerRank/Stacks/stack.py
@@ -0,0 +1,24 @@
+class Stack(object):
+    def __init__(self):
+        # in python we implement stacks using list
+        self.items = list()
+
+    def __len__(self):
+        return self.size()
+     
+    def size(self):
+        return len(self.items)
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):  
+        if not self.is_empty():
+            return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def is_empty(self):
+        return len(self.items) == 0
